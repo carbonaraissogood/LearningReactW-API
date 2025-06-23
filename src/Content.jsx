9dee3af9567a659e273f96dde6d10bcb184d8fa4
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-06-23 15:54:38 +0800

    Add Content component for grocery list with check and delete functionality

---
+++ src/Content.jsx
@@ -0,0 +1,39 @@
+import { FaTrashAlt } from 'react-icons/fa';
+
+const Content = ({ items, handleCheck, handleDelete }) => {
+
+  return (
+    <main>
+      {items.length ? (
+
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+
+              <label
+                style={(item.checked) ? {textDecoration: 'line-through'} : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >{item.item}</label>
+
+              <FaTrashAlt
+                onClick={() => handleDelete(item.id)}
+                role='button'
+                tabIndex='0'>
+              </FaTrashAlt>
+
+            </li>
+          ))}
+        </ul>
+      ) : <p>No items on the grocery list.</p>
+    }  
+    </main>
+  )
+}
+
+export default Content
